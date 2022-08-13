5c31841ef635da187b41e34224c055a1a68616a2
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2022-08-13 13:01:39 +0600

    null pointer exception solved

--- app/src/main/java/com/example/debugging/MainActivity.kt
+++ app/src/main/java/com/example/debugging/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.debugging
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 
 private const val TAG = "MainActivity"
 
@@ -30,6 +31,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val helloTextView: TextView = findViewById(R.id.hello_world)
+        helloTextView.text = "Hello, Debugging!"
         logging()
         division()
     }
